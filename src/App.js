c07e021a33307bd45f93b216941f58f36fc3c150
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-09-27 21:57:54 -0400

    Added Header and input form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React, { Component } from "react";
+
+import Header from "./components/Header";
+import Search from "./components/Search";
+
 import "./App.css";
 
 class App extends Component {
+  state = {
+    artistInput: "",
+    songInput: ""
+  };
+
+  onInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  searchSong = e => {
+    e.preventDefault();
+  };
+
   render() {
-    return;
+    return (
+      <div>
+        <Header />
+        <Search
+          onInputChange={this.onInputChange}
+          searchSong={this.searchSong}
+        />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <h1>Lyric Finder</h1>
+        <p>Write the song and artist and get the lyrics!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
